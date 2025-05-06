e5910c3dec5cc81fec0d2a2cc57b05e252da864d
Chael250 <ganzac784@gmail.com>
2025-05-06 06:43:05 +0200

    Implementation of multi-selection

--- src/components/accordions/index.jsx
+++ src/components/accordions/index.jsx
@@ -1,9 +1,44 @@
+import data from "./data";
+import { useState } from "react";
+import "./styles.css";
+
 export default function Accordion() {
     const [selected, setSelected] = useState(null);
+    const [multiSelect, setMultiSelect] = useState(false);
+    const [selectedItems, setSelectedItems] = useState([]);
+
+    function handleSingleSelection(getCurrentId) {
+        setSelected(getCurrentId === selected ? null : getCurrentId);
+    }
+
+    function handleMultiSelection(getCurrentId) {
+        if(selectedItems.includes(getCurrentId)) {
+            setSelectedItems(selectedItems.filter(item => item !== getCurrentId));
+        } else {
+            setSelectedItems([...selectedItems, getCurrentId]);
+        }
+    }
+
     return (
-        <div className="wrapper">
-            <div className="accordian">
-                
+        <div className="flex flex-col h-[100vh] w-[100vw] justify-center items-center gap-10">
+            <button onClick={() => setMultiSelect(!multiSelect)} className="w-[200px] h-[50px] bg-[#614101] rounded-2xl text-white cursor-pointer">Enable Multi Selection</button>
+            <div className="w-[500px]">
+                {
+                    data && data.length > 0 ?
+                    data.map(dataItem => <div className="bg-[#614101] mb-[10px] px-[10px] py-[20px]">
+                        <div onClick={multiSelect ? () => handleMultiSelection(dataItem.id) : () => handleSingleSelection(dataItem.id)} className="text-white flex justify-between items-center cursor-pointer">
+                            <h3>{dataItem.question}</h3>
+                            <span>+</span>
+                        </div>
+                        {
+                            selected === dataItem.id ? 
+                            <div className="text-white h-auto mt-[10px]">{dataItem.answer}</div>
+                            : null
+                        }
+                    </div>
+                    ) : (
+                    <div>No data found!</div>
+                )}
             </div>
         </div>
     )
